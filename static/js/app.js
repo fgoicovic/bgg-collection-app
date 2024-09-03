eafe65c3e8fcfbf67715430b8657ff39245f2a42
Felipe Goicovic <felipe.goicovic@sap.com>
2024-09-03 15:47:44 +0200

    Ordering list of game alphabetically

--- static/js/app.js
+++ static/js/app.js
@@ -17,16 +17,32 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     function displayGames(username, games) {
+        // Clear the games list container
         gamesListContainer.innerHTML = '';
-        for (const gameId in games){
+    
+        // Convert the games object to an array of game entries and sort them alphabetically by name
+        const sortedGames = Object.entries(games).sort((a, b) => a[1].name.localeCompare(b[1].name));
+    
+        // Loop through each sorted game entry
+        for (const [gameId, game] of sortedGames) {
+            // Create a new div element for the game
             const gameDiv = document.createElement('div');
+            
+            // Add the 'game' class to the div
             gameDiv.classList.add('game');
-            gameDiv.innerText = games[gameId].name + " (" + games[gameId].year_published + ")";
+            
+            // Set the inner text of the div to the game's name and year published
+            gameDiv.innerText = `${game.name} (${game.year_published})`;
+            
+            // Add a click event listener to the div
             gameDiv.addEventListener('click', function() {
+                // Fetch game details from the server
                 fetch(`/api/${username}/game_details/${gameId}`)
-                    .then(response => response.json())
-                    .then(details => displayGameDetails(details, gameId));
+                    .then(response => response.json()) // Convert the response to JSON
+                    .then(details => displayGameDetails(details, gameId)); // Call displayGameDetails with the details and gameId
             });
+            
+            // Append the div to the games list container
             gamesListContainer.appendChild(gameDiv);
         }
     }
